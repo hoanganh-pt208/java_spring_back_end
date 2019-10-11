321a37320287208d392c0625411947529344eb70
hoanganh-PC\hoanganh <anhpt@gemiso.com>
2019-10-11 15:30:56 +0700

    make token by Oauth2

--- src/main/java/com/hoanganh/config/CORSFilterConfig.java
+++ src/main/java/com/hoanganh/config/CORSFilterConfig.java
@@ -11,6 +11,7 @@ public class CORSFilterConfig extends WebMvcConfigurerAdapter{
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+        .allowedOrigins("*")
                 .allowedMethods("GET", "POST","DELETE","PUT");
     }
 }
---
+++ src/main/java/com/hoanganh/security/FilterChainInitializer.java
@@ -0,0 +1,7 @@
+package com.hoanganh.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class FilterChainInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
